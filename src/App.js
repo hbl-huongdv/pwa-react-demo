8d7b8a820a39ea032540c7a230dd0da7e452539a
huongdv <huongdv@hblab.vn>
2019-05-06 15:06:04 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="wistia_embed wistia_async_3z5o1eujqh" style={{ height: "349px", width: "620px" }}>&nbsp;</div>
+    </div>
+  );
+}
+
+export default App;
